dad67f54b3126e72d943a8fd5e684c8417ef2f51
Philip Craig <philipjcraig@gmail.com>
2020-02-02 15:12:56 +1000

    Handle indirect memory calls
    This should be able to handle GOT calls.

--- src/code.rs
+++ src/code.rs
@@ -1,8 +1,10 @@
 use capstone::arch::x86::X86OperandType;
+use capstone::arch::x86::X86Reg::*;
 use capstone::arch::ArchOperand;
 use capstone::{self, Arch, Capstone, Insn, InsnDetail, InsnGroupType, Mode};
 use object::target_lexicon::Architecture;
 use object::{File, Object, ObjectSegment};
+use std::convert::TryInto;
 
 #[derive(Debug)]
 pub(crate) struct Code<'code> {
@@ -46,13 +48,21 @@ impl<'code> Code<'code> {
         })
     }
 
-    pub(crate) fn calls<F>(&self, begin: u64, end: u64, f: F)
+    pub(crate) fn calls<F>(&self, begin: u64, end: u64, mut f: F) -> Option<()>
     where
         F: FnMut(u64, u64),
     {
-        if let Some(code) = self.range(begin, end) {
-            calls(self.arch, self.mode, code, begin, f);
+        if let Some(range) = self.range(begin, end) {
+            let mut cs =
+                Capstone::new_raw(self.arch, self.mode, capstone::NO_EXTRA_MODE, None).ok()?;
+            cs.set_detail(true).ok()?;
+            for insn in cs.disasm_all(range, begin).ok()?.iter() {
+                if let Some(call) = call(self, &cs, &insn) {
+                    f(call.from, call.to);
+                }
+            }
         }
+        Some(())
     }
 
     fn range(&self, begin: u64, end: u64) -> Option<&'code [u8]> {
@@ -67,28 +77,14 @@ impl<'code> Code<'code> {
     }
 }
 
-fn calls<F>(arch: Arch, mode: Mode, code: &[u8], addr: u64, mut f: F) -> Option<()>
-where
-    F: FnMut(u64, u64),
-{
-    let mut cs = Capstone::new_raw(arch, mode, capstone::NO_EXTRA_MODE, None).ok()?;
-    cs.set_detail(true).ok()?;
-    for insn in cs.disasm_all(code, addr).ok()?.iter() {
-        if let Some(call) = call(arch, &cs, &insn) {
-            f(call.from, call.to);
-        }
-    }
-    Some(())
-}
-
-fn call(arch: Arch, cs: &Capstone, insn: &Insn) -> Option<Call> {
-    match arch {
-        Arch::X86 => call_x86(cs, insn),
+fn call(code: &Code, cs: &Capstone, insn: &Insn) -> Option<Call> {
+    match code.arch {
+        Arch::X86 => call_x86(code, cs, insn),
         _ => None,
     }
 }
 
-fn call_x86(cs: &Capstone, insn: &Insn) -> Option<Call> {
+fn call_x86(code: &Code, cs: &Capstone, insn: &Insn) -> Option<Call> {
     let detail = cs.insn_detail(insn).ok()?;
     if !is_call(&detail) {
         return None;
@@ -96,11 +92,34 @@ fn call_x86(cs: &Capstone, insn: &Insn) -> Option<Call> {
     let arch_detail = detail.arch_detail();
     for op in arch_detail.operands() {
         if let ArchOperand::X86Operand(op) = op {
-            if let X86OperandType::Imm(imm) = op.op_type {
-                return Some(Call {
-                    from: insn.address(),
-                    to: imm as u64,
-                });
+            match op.op_type {
+                X86OperandType::Imm(imm) => {
+                    return Some(Call {
+                        from: insn.address(),
+                        to: imm as u64,
+                    });
+                }
+                X86OperandType::Mem(op) => {
+                    let base = op.base().0 as u32;
+                    if base == X86_REG_RIP || base == X86_REG_EIP {
+                        let from = insn.address();
+                        let mem = (from + insn.bytes().len() as u64).wrapping_add(op.disp() as u64);
+                        if base == X86_REG_RIP {
+                            if let Some(range) = code.range(mem, mem + 8) {
+                                let to = u64::from_le_bytes(range.try_into().unwrap());
+                                return Some(Call { from, to });
+                            }
+                        } else {
+                            if let Some(range) = code.range(mem, mem + 8) {
+                                let to = u32::from_le_bytes(range.try_into().unwrap()) as u64;
+                                return Some(Call { from, to });
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    // TODO: can we do anything with indirect register calls?
+                }
             }
         }
     }
